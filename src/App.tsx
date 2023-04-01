d7a8644df396bd613dcea46cce91ad1912d0cc8b
sw1227 <32032108+sw1227@users.noreply.github.com>
2023-04-01 15:11:21 +0900

    feat: simple gradient descent from clicked position

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import mapboxgl, { MapboxOptions } from 'mapbox-gl'
 import './App.css'
-import { LngLat, getTileCoordFromPixel, lngLatToPixel, getPixelInTile } from './geo_util'
+import { lngLatToPixel, gradientDescent, PixelCoord, pixelToLngLat } from './geo_util'
 
 const options: MapboxOptions = {
   accessToken: import.meta.env.VITE_MAPBOX_TOKEN,
@@ -10,24 +10,6 @@ const options: MapboxOptions = {
   localIdeographFontFamily: "'Noto Sans', 'Noto Sans CJK SC', sans-serif",
   center: [139.7, 35.7],
   zoom: 12
-}
-
-// Given lng/lat position, return dem tile data and index of the point in the tile
-const fetchTile = async (lngLat: LngLat): Promise<{ tile: number[][], indexInTile: { x: number, y: number } }> => {
-  // Conevert to tile coordinate
-  const zoom = 13;
-  const pixelCoord = lngLatToPixel(lngLat, zoom)
-  const tileCoord = getTileCoordFromPixel(pixelCoord)
-
-  // Fetch dem tile data
-  const url = `https://cyberjapandata.gsi.go.jp/xyz/dem/${tileCoord.z}/${tileCoord.x}/${tileCoord.y}.txt`
-  const text = await fetch(url).then(res => res.text());
-  const rows = text.split('\n')
-  const tile = rows
-    .slice(0, rows.length - 1) // last row is empty
-    .map(r => r.split(',').map(d => d === 'e' ? 0 : parseFloat(d))) // e: sea
-
-  return { tile, indexInTile: getPixelInTile(pixelCoord) }
 }
 
 const useMapboxMap = (options: MapboxOptions) => {
@@ -40,12 +22,30 @@ const useMapboxMap = (options: MapboxOptions) => {
     // Add click event listener to the map
     map.on('click', async (e) => {
 
-      const { tile, indexInTile } = await fetchTile(e.lngLat);
-      console.log(`TODO: height: ${tile[indexInTile.y][indexInTile.x]}`)
-
+      // Add a marker to the map, indicating the clicked location
       const marker = new mapboxgl.Marker()
         .setLngLat(e.lngLat)
         .addTo(map);
+
+      // Start gradient descent
+      const zoom = 13
+      const epsilon = 0.3
+      const maxStep = 10
+      let position: PixelCoord = lngLatToPixel(e.lngLat, zoom)
+      const locationHistory: PixelCoord[] = [{ ...position }]
+      for (let i = 0; i < maxStep; i++) {
+        position = await gradientDescent(position, epsilon)
+        locationHistory.push({ ...position })
+      }
+
+      console.log('TODO: ', locationHistory)
+      // Create markers for each location in the location history
+      locationHistory.map(p => {
+        const lngLat = pixelToLngLat(p)
+        const marker = new mapboxgl.Marker()
+          .setLngLat(lngLat)
+          .addTo(map);
+      })
     })
     setMap(map)
   }, [])
--- src/geo_util.ts
+++ src/geo_util.ts
@@ -73,3 +73,67 @@ export const getPixelInTile = (pixel: PixelCoord): { x: number, y: number } => {
     y: pixel.y % tileSize
   }
 }
+
+const fetchTile = async (tileCoord: TileCoord): Promise<number[][]> => {
+  const url = `https://cyberjapandata.gsi.go.jp/xyz/dem/${tileCoord.z}/${tileCoord.x}/${tileCoord.y}.txt`
+
+  const text = await fetch(url).then(res => res.text());
+  const rows = text.split('\n')
+  const tile = rows
+    .slice(0, rows.length - 1) // last row is empty
+    .map(r => r.split(',').map(d => d === 'e' ? 0 : parseFloat(d))) // e: sea
+  return tile
+}
+
+// Get elevation at given pixel coordinate
+export const getElevation = async (pixelCoord: PixelCoord): Promise<number> => {
+  const tileCoord = getTileCoordFromPixel(pixelCoord)
+  const tile = await fetchTile(tileCoord)
+  const { x, y } = getPixelInTile(pixelCoord)
+  return tile[y][x]
+}
+
+// Unit: [meter / pixel]
+type Gradient = {
+  dx: number
+  dy: number
+}
+// Get gradient at given pixel coordinate
+export const getGradient = async (pixelCoord: PixelCoord): Promise<Gradient> => {
+
+  const fx = Math.floor(pixelCoord.x)
+  const fy = Math.floor(pixelCoord.y)
+
+  if ((pixelCoord.x - fx) + (pixelCoord.y - fy) < 1) {
+    const [nwElev, neElev, swElev] = await Promise.all([
+      getElevation({ x: fx, y: fy, z: pixelCoord.z }),
+      getElevation({ x: fx + 1, y: fy, z: pixelCoord.z }),
+      getElevation({ x: fx, y: fy + 1, z: pixelCoord.z }),
+    ])
+    return {
+      dx: neElev - nwElev,
+      dy: swElev - nwElev,
+    }
+  } else {
+    const [neElev, swElev, seElev] = await Promise.all([
+      getElevation({ x: fx + 1, y: fy, z: pixelCoord.z }),
+      getElevation({ x: fx, y: fy + 1, z: pixelCoord.z }),
+      getElevation({ x: fx + 1, y: fy + 1, z: pixelCoord.z }),
+    ])
+    return {
+      dx: seElev - swElev,
+      dy: seElev - neElev,
+    }
+  }
+};
+
+// Get next pixel coordinate by gradient descent
+export const gradientDescent = async (startPixel: PixelCoord, epsilon: number): Promise<PixelCoord> => {
+  const gradient = await getGradient(startPixel)
+  const nextPixel = {
+    x: startPixel.x - epsilon * gradient.dx,
+    y: startPixel.y - epsilon * gradient.dy,
+    z: startPixel.z,
+  }
+  return nextPixel
+};
